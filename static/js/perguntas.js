a47055b49a1aeab4e992963072215f4082d89fa3
Gokur <fdfelipe33@gmail.com>
2024-11-02 13:02:19 -0300

    Add files via upload

---
+++ static/js/perguntas.js
@@ -0,0 +1,21 @@
+const palavras = [
+    "Coulomb",
+    "Campo elétrico",
+    "Potencial",
+    "Eletrização",
+    "Condutor",
+    "Repulsão",
+    "Atração",
+    "Indução",
+];
+
+const dicas = [
+    "Unidade de carga elétrica e nome da lei de força entre cargas.",
+    "Região de influência ao redor de uma carga.",
+    "Refere-se ao potencial elétrico.",
+    "Processo de transferência de cargas elétricas.",
+    "Material que permite o movimento de cargas elétricas.",
+    "Fenômeno em que cargas de mesmo sinal se repelem.",
+    "Fenômeno em que cargas de sinais opostos se atraem.",
+    "Método de eletrização onde um objeto carrega outro sem contato direto.",
+]
